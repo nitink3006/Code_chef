ecad34e876c6e404fd44c1712963c12b5afba25a
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2022-10-19 21:42:35 +0530

    Create timely.cpp

---
+++ timely.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        int x;
+        cin>>x;
+        if(x>=30){
+            cout<<"YES"<<endl;
+        }
+        else if(x<=30){
+            cout<<"NO"<<endl;
+        }
+    }
+	return 0;
+}
+
+
+
+#Questions
+
+https://www.codechef.com/START61D/problems/TIMELY
