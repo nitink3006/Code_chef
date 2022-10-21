cce142376d92f1086f06db88156e0b2ae0642fbf
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2022-10-21 12:37:34 +0530

    Create paper_cutting.cpp

---
+++ paper_cutting.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int size,paper;
+	    cin>>paper>>size;
+	    int square=(paper)/(size);
+	    cout<<square*square<<endl;
+	}
+	return 0;
+}
+
+
+#Questions
+https://www.codechef.com/submit/CUTPAPER?tab=statement
