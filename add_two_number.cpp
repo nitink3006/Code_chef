5b083fb06a0e1c14886ef29c50526d794e68a7ec
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2022-10-21 12:41:54 +0530

    Create add_two_number.cpp

---
+++ add_two_number.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	for (int i = 1; i <= T; i++) {
+		int a, b;
+		cin >> a >> b;
+		int ans = a + b;
+		cout << ans << "\n";
+	}
+
+	return 0;
+}
+
+
+#Question
+https://www.codechef.com/submit/FLOW001
