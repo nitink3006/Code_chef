90491b0bb112178f528900ba18afd4c91ea59e57
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2022-10-21 12:25:30 +0530

    Create expert_setter.cpp

---
+++ expert_setter.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        float x,y;
+        cin>>x>>y;
+        if(((y/x)*100)>=50){
+            cout<<"YES"<<endl;
+        }
+        else {
+            cout<<"NO"<<endl;
+        }
+        
+    }
+	
+	return 0;
+}
+
+
+#Questions
+https://www.codechef.com/submit/EXPERT?tab=statement
