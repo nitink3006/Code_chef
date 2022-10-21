0de6da36d164bfdc88b0d5c5f51e3c6073d4873f
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2022-10-21 12:52:07 +0530

    Create richie_rich.cpp

---
+++ richie_rich.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b,y;
+	    cin>>a>>b>>y;
+	    cout<<(b-a)/y<<endl;
+	}
+	return 0;
+}
+
+
+
+#Questions
+https://www.codechef.com/LP1TO201/problems/CHFRICH
