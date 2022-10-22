f5988897adafa173776e00db45d30e50e416cc31
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2022-10-22 09:51:49 +0530

    Create hoop_jump.cpp

---
+++ hoop_jump.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    if(n%2!=0){
+	        cout<<(n+1)/2<<endl;
+	    }
+	}
+	return 0;
+}
+
+
+
+#Questions
+https://www.codechef.com/LP1TO201/problems/HOOPS?tab=statement
