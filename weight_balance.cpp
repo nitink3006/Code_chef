b62071a818089ffce6a4ebe566c06d545e48ef89
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2022-10-31 16:50:13 +0530

    Create weight_balance.cpp

---
+++ weight_balance.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int w1,w2,x1,x2,m;
+        cin>>w1>>w2>>x1>>x2>>m;
+        if((w2-w1)>=(x1*m) && (w2-w1)<=(x2*m)){
+            cout<<"1"<<endl;
+        }
+        else {
+           cout<<"0"<<endl;
+        }
+    }
+	return 0;
+}
+
+
+
+#Questions
+https://www.codechef.com/LP1TO201/problems/WEIGHTBL?tab=statement
