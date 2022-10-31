8a30d24f434d57e5ed798972edc880e64f0668cc
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2022-10-31 14:19:52 +0530

    Create small_whole.cpp

---
+++ small_whole.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,k;
+	    cin>>n>>k;
+	    if(n>0 && k>0){
+	        cout<<(n%k)<<endl;
+	    }
+	    else   cout<<n<<endl;
+	}      
+	return 0;
+}
+
+
+
+#Questions
+https://www.codechef.com/LP1TO201/problems/SMOL?tab=statement
