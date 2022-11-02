49be3791f5a0e9e5747abea814d1b7b2a22bd75f
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2022-11-02 19:48:08 +0530

    Create manysums.cpp

---
+++ manysums.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b;
+	    cin>>a>>b;
+	    cout<<2*(b-a)+1<<endl;
+	}
+	return 0;
+}
+
+
+
+#Questions
+https://www.codechef.com/LP1TO201/problems/MANYSUMS?tab=statement
