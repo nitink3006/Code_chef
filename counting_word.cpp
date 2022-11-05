0c8eae27a85691c5a0d781bbed812368ee2fdd9d
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2022-11-05 20:54:10 +0530

    Create counting_word.cpp

---
+++ counting_word.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+    cin>>t;
+    while(t--){
+        int n,m;
+        cin>>n>>m;
+        cout<<n*m<<endl;
+    }
+	return 0;
+}
+
+
+#Question
+https://www.codechef.com/submit/CNTWRD
