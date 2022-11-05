1e008615b7773681f863bea6015f28875c640cd8
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2022-11-05 15:25:44 +0530

    Create Roller_coster.cpp

---
+++ Roller_coster.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,h;
+	    cin>>x>>h;
+	    if(x<h)
+	    {
+	        cout<<"No"<<endl;
+	    }
+	    else cout<<"Yes"<<endl;
+	}
+	return 0;
+}
+
+
+
+
+#Questions
+https://www.codechef.com/submit/MINHEIGHT
