0fd77cd70ad54997e2bed4c7801593014cacd7ef
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2022-11-05 15:13:43 +0530

    Create taller.cpp

---
+++ taller.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    if(x<y && x!=y){
+	        cout<<"B"<<endl;
+	    }
+	    else cout<<"A"<<endl;
+	}	
+	return 0;
+}
+
+
+
+
+#Questions
+https://www.codechef.com/submit/TALLER
