2c3d3b98975a80470a0f30d50d06ddaaa297a237
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2022-11-05 21:23:14 +0530

    Create chess_match.cpp

---
+++ chess_match.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	    cout<<2*(180+a)-(b+c)<<endl;
+	}
+	return 0;
+}
+
+
+
+
+#Question
+https://www.codechef.com/submit/BLITZ3_2
