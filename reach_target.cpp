05b0aac3e901b2ffdce95f2369e8540121d98f70
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2022-11-05 15:10:10 +0530

    Create reach_target.cpp

---
+++ reach_target.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int y,x;
+	    cin>>y>>x;
+	    cout<<y-x<<endl;
+	}
+	return 0;
+}
+
+
+
+#Questions
+https://www.codechef.com/submit/REACHTARGET
+
