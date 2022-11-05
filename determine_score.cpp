0d473032928787d09295e31009a029d902e9b29b
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2022-11-05 14:59:36 +0530

    Create determine_score.cpp

---
+++ determine_score.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,n;
+	    cin>>x>>n;
+	    cout<<x/10*n<<endl;
+	}
+	return 0;
+}
+
+
+
+#Questions
+https://www.codechef.com/submit/DETSCORE
