a289dfeb06c44786fd2c935ee50626d78e6df2fe
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2022-11-05 21:04:21 +0530

    Create prizemoney.cpp

---
+++ prizemoney.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int y,x;
+	    cin>>y>>x;
+	    cout<<10*y+90*x<<endl;
+	}
+	return 0;
+}
+
+
+
+#Questions
+https://www.codechef.com/submit/PRIZEPOOL
