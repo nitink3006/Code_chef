cdfe5df176b5b9f54d8405e90ff559a9d8b76727
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2022-11-09 18:53:53 +0530

    Create squares_triangle.cpp

---
+++ squares_triangle.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int b,n;
+	    cin>>b;
+	    n=(b-2)/2;
+        cout<<(n*(n+1))/2<<endl;
+	}
+	return 0;
+}
+
+
+#Question
+https://www.codechef.com/submit/TRISQ
