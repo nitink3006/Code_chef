61e64bf72d62f2f00ca9c09abffe1268acddebd3
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2022-11-11 18:49:44 +0530

    Create chef_party.cpp

---
+++ chef_party.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,x,k;
+	    cin>>n>>x>>k;
+	    if((x*n)<=k){
+	        cout<<"Yes"<<endl;
+	    }
+	    else cout<<"No"<<endl;
+	}
+	return 0;
+}
+
+
+
+#Question
+https://www.codechef.com/submit/PARTY2
