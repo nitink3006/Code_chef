f127c664f22edf268113e874fec1a9497a99a037
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2022-11-20 16:55:59 +0530

    Create air_hockey.cpp

---
+++ air_hockey.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b;
+	    cin>>a>>b;
+	    if(a<=b){
+	        cout<<7-b<<endl;
+	    }
+	    else cout<<7-a<<endl;
+	}
+	return 0;
+}
+
+
+
+#Question
+https://www.codechef.com/problems/AIRHOCKEY?tab=statement
