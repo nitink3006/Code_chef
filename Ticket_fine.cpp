ca5099a528f464a4a35d26e4637e03b9962fb41f
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2022-11-22 22:45:14 +0530

    Create Ticket_fine.cpp

---
+++ Ticket_fine.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,q,p;
+	    cin>>x>>q>>p;
+	    cout<<(q-p)*x<<endl;
+	}
+	return 0;
+}
+
+
+
+#Question
+https://www.codechef.com/problems/TCKTFINE
