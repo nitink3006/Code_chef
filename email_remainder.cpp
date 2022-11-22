734b831b5056ca80420a0b07b6ec216a17fd4cb1
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2022-11-23 00:24:39 +0530

    Create email_remainder.cpp

---
+++ email_remainder.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	    int u,n;
+	    cin>>u>>n;
+	    cout<<u-n<<endl;
+	     
+	return 0;
+}
+
+
+#Question
+https://www.codechef.com/problems/EMAILREM?tab=statement
