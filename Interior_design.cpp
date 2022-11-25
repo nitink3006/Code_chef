214611efd04a6c13297a1a0f1bbfe5b9ab3b1f04
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2022-11-25 22:47:13 +0530

    Create Interior_design.cpp

---
+++ Interior_design.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b,c,d;
+	    cin>>a>>b>>c>>d;
+	    if((a+b)<(c+d)){
+	        cout<<(a+b)<<endl;
+	    }
+	    else cout<<(c+d)<<endl;
+	}
+	return 0;
+}
+
+
+
+#Question
+https://www.codechef.com/problems/INTRDSGN
