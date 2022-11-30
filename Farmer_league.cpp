b3fd60f16c5482f1907249a0b836f1c0698a69c7
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2022-11-30 19:21:28 +0530

    Create Farmer_league.cpp

---
+++ Farmer_league.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+     int t;
+     cin>>t;
+     while(t--){
+         int n;
+         cin>>n;
+         cout<<(n/2)*3<<endl;
+     }
+     return 0;
+}
+
+
+
+#Question
+https://www.codechef.com/problems/LEAGUE?tab=statement
