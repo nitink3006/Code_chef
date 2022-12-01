6d95a79dfb569f45dbb476443b255722d32a8290
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2022-12-01 12:44:17 +0530

    Create Summer_heat.cpp

---
+++ Summer_heat.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b,c,d;
+	    cin>>a>>b>>c>>d;
+	    cout<<(c/a)+(d/b)<<endl;
+	}
+	return 0;
+}
