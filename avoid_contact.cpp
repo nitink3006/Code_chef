c8b678e2424faebd06ca52755033b59751a6f8c7
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2022-12-02 09:55:12 +0530

    Create avoid_contact.cpp

---
+++ avoid_contact.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>y>>x;
+	    if(y==x){
+	        cout<<(y+x)-1<<endl;
+	    }
+	    else cout<<y+x<<endl;
+	}
+	return 0;
+}
+
+
+
+
+#Question
+https://www.codechef.com/problems/AVOIDCONTACT
