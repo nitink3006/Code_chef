d6072b5f97ea4875aef6868fddefcc2c7cc857bc
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2022-12-26 22:57:42 +0530

    Create chef_contest.cpp

---
+++ chef_contest.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,r;
+	cin>>t>>r;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    if(n<r){
+	        cout<<"Bad boi"<<endl;
+	    }
+	    else cout<<"Good boi"<<endl;
+	}
+	return 0;
+}
+
+
+
+#Question
+https://www.codechef.com/problems/CHFINTRO
