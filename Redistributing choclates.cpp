ef1cc4bafe268e1536b4c2f8de1281dff542861c
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2023-02-04 17:06:56 +0530

    Create Redistributing choclates.cpp

---
+++ Redistributing choclates.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	    int s=a+b+c;
+	    if(s>=6) cout<<"YES"<<endl;
+	    else cout<<"NO"<<endl;
+	}
+	return 0;
+}
+
+
+#Question
+https://www.codechef.com/problems/REDSTRBTN?tab=statement
