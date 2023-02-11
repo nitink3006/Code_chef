4fd0d20ac55d5b6914a3ee3a5ea1cb7372a8841a
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2023-02-11 15:39:24 +0530

    Create Permutation and Modulus.cpp

---
+++ Permutation and Modulus.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    for(int i=2;i<=n;i++){
+	        cout<<i<<" ";
+	    }
+	    cout<<1<<endl;
+	}
+	return 0;
+}
+
+
+#Question
+https://www.codechef.com/problems/PER_MOD?tab=statement
