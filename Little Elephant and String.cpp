f2b40ec75e61a27f78755d01f5772d6bc8df1d16
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2023-02-13 23:17:16 +0530

    Create Little Elephant and String.cpp

---
+++ Little Elephant and String.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int k,n;
+	cin>>k>>n;
+	string a[k],b[n];
+	for(int i=0;i<k;i++)cin>>a[i];
+	for(int i=0;i<n;i++)cin>>b[i];
+	for(int i=0;i<n;i++){
+	    int c=0;
+	    for(int j=0;j<k;j++){
+	        if (b[i].find(a[j]) != std::string::npos) 
+	           { cout<<"Good"<<endl;c=1;break;}
+	          
+	       }
+	       if(c==0){
+	       if(b[i].length()>=47)
+	        cout<<"Good"<<endl;
+	        else 
+	       cout<<"Bad"<<endl;}
+	}
+	return 0;
+}
+
+
+
+#Question
+https://www.codechef.com/problems/LUCKYSTR?tab=statement
