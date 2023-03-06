7cf5cf2780054e323a93cf2e030410fd46b35099
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2023-03-06 22:01:18 +0530

    Create Pushpa.cpp

---
+++ Pushpa.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    int arr[x];
+	    map<int,int>mp;
+	   for(int i=0;i<x;i++){
+	       cin>>arr[i];
+	       mp[arr[i]]++;
+	   }
+	   int sum=0;
+	   for(auto i : mp){
+	       sum =max(sum,(i.first+i.second-1));
+	   }
+	   cout<<sum<<endl;
+	}
+	return 0;
+}
+
+
+#Question
+https://www.codechef.com/problems/PUSH7PA?tab=statement
